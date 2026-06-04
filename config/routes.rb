bc3b7092eacb7a8f00ab3d6329ee117ec09535e6
saka-ri08 <262088770+saka-ri08@users.noreply.github.com>
2026-06-04 19:52:47 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   root "homes#top"
 
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "home", to: "homes#show", as: :home
 
   resources :tasks
+
+  get "calendar", to: "calendars#show"
+  get "calendar/:date", to: "calendar_days#show", as: :calendar_day
+  get "free_tasks", to: "free_tasks#index"
+
+  get "up" => "rails/health#show", as: :rails_health_check
 end
